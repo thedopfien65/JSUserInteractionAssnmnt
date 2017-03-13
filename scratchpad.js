3346e6c82a0218fd4ea29367d13e19127b33b872
Bryan Burnham <bryan.d.burnham@gmail.com>
2017-03-12 21:26:52 -0600

    At least the numbers work

--- scratchpad.js
+++ scratchpad.js
@@ -1,3 +1,4 @@
+//fibonacci functions
 function fibby(n){
 	var value;
 	var div = document.createElement('div');
@@ -25,9 +26,73 @@ function fibby(n){
 	return {'value':value, 'html':div};
 
 }
-
 var fib = function(n, node){
 	var tree = fibby(n);
+	node.appendChild(tree.html);
+}
+
+
+//pell functions
+function pelly(n){
+	var value;
+	var div = document.createElement('div');
+
+	if(n<2){
+		if(n===0){
+			value = 0;
+		} else {
+			value = 1;
+		}
+		var p = document.createElement(p);
+		p.textContent = "Pell("+n+") ="+value;
+		div.appendChild(p);
+	}else{
+		var left = pelly(n-1);
+		var right = pelly(n-2);
+		value = 2*(left.value) + right.value;
+		var p = document.createElement(p);
+		p.textContent = "Pell("+n+") ="+value;
+		div.appendChild(p);
+		div.appendChild(left.html);
+		div.appendChild(right.html);
+	}	
+	return {'value':value, 'html':div};
+}
+var pell = function(n, node){
+	var tree = pelly(n);
+	node.appendChild(tree.html);
+}
+
+//tribonacci functions
+function trelly(n){
+	var value;
+	var div = document.createElement('div');
+
+	if (n < 3){
+		if(n===2){
+			value = 1;
+		} else {
+			value = 0;
+		}
+		var p = document.createElement(p);
+		p.textContent = "Trib("+n+") ="+value;
+		div.appendChild(p);
+	}else{
+		var left = trelly(n-1);
+		var mid = trelly(n-2);
+		var right = trelly(n-3);
+		value = left.value + mid.value + right.value;
+		var p = document.createElement(p);
+		p.textContent = "Trib("+n+") ="+value;
+		div.appendChild(p);
+		div.appendChild(left.html);
+		div.appendChild(mid.html);
+		div.appendChild(right.html);
+	}
+	return {'value':value, 'html':div};
+}
+var trib = function(n, node){
+	var tree = trelly(n);
 	node.appendChild(tree.html);
 }
 
@@ -40,3 +105,5 @@ document.body.appendChild(tribDiv);
 document.body.appendChild(pellDiv);
 
 fib(11, fibDiv);
+pell(11, pellDiv);
+trib(11, tribDiv);
